2f51fd12f29025eccac8c7a8f2cefefac097a417
SergioGomez60 <sergio60gomez@gmail.com>
2025-11-13 18:17:11 +0100

    Empezando la seccion 1

--- src/app/components/seccion1/seccion1.ts
+++ src/app/components/seccion1/seccion1.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './seccion1.css',
 })
 export class Seccion1 {
-
+  camisetas = [
+    {equipo: "FC Barcelona",descripcion:"Camiseta del FC Barcelona 2025/2026",precio:89.99,imagen:"assets/barcelona.jpg"},
+    {equipo: "Real Madrid",descripcion:"Camiseta del Real Madrid 2025/2026",precio:89.99,imagen:"assets/madrid.jpg"},
+    {equipo: "Liverpool FC",descripcion:"Camiseta del Liverpool FC 2025/2026",precio:89.99,imagen:"assets/liverpool.jpg"},
+    {equipo: "Arsenal",descripcion:"Camiseta del Arsenal 2025/2026",precio:89.99,imagen:"assets/arsenal.jpg"}
+  ]
 }
