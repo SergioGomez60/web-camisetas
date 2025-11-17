ecd521dd416bf0a749d6c9d5e3e346c36934b032
SergioGomez60 <sergio60gomez@gmail.com>
2025-11-17 10:53:56 +0100

    111

--- backend/routes/usuarios.js
+++ backend/routes/usuarios.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { promiseConexion } from "../db.js";
 import bcrypt from "bcryptjs";
+import { validateUser } from "../validaciones/usuarios.js";
 
 const router = Router();
 
@@ -16,11 +17,20 @@ router.get("/", async (req,res) =>{
     
 })
 
-router.post("/usuarios",async (req,res)=>{
+router.post("/registro",validate(validateUser),async (req,res)=>{
     try{
         const {name,email,password} = req.body
-    }catch{
 
+        // Chequeo de email ya registrado
+        const [existeEmail] = await promiseConexion.query("SELECT id FROM users WHERE email = ? LIMIT 1", [email]);
+        if(existeEmail.length > 0) return res.status(409).json({ message: "El email ya está registrado" });
+
+        const passwordHashed = await bcrypt.hash(password,10); // transforma la contraseña en un hash seguro. El número 10 es el salt rounds, suficiente para seguridad sin perder rendimiento.
+        const [result] = await promiseConexion.query("INSERT INTO users (username, email, password) VALUES (?, ?, ?)",[username, email, passwordHashed]);
+        res.status(201).json({ message: "Usuario creado", id: result.insertId });
+    }catch (err){
+        console.error(err);
+        res.status(500).json({ message: "Error interno" });
     }
 })
 
--- backend/validaciones/usuarios.js
+++ backend/validaciones/usuarios.js
@@ -1,1 +1,22 @@
 import z from "zod";
+
+const validacionUsuario = z.object({
+    username: z
+    .string()
+    .min(3, { message: "El nombre de usuario debe tener al menos 3 caracteres"})
+    .max(30, { message:"El nombre de usuario puede tener como máximo 30 caracteres"})
+    .regex(/^[a-zA-Z0-9_.-]+$/, { message:"El nombre de usuario solo puede contener letras, números, _ . -"}),
+
+    email: z.string().email({ message: "Introduce un email válido" }),
+
+    password: z.string().min(8, { message: "La contraseña debe tener al menos 8 caracteres" }) .max(128, { message: "La contraseña es demasiado larga" })
+    // Aquí el .refine también se puede usar con el objeto, aunque con string funciona.
+    .refine(val => /[a-z]/.test(val), { message: "La contraseña debe contener al menos una letra minúscula" })
+    .refine(val => /[A-Z]/.test(val), { message: "La contraseña debe contener al menos una letra mayúscula" })
+    .refine(val => /[0-9]/.test(val), { message: "La contraseña debe contener al menos un número" })
+    .refine(val => /[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(val), { message: "La contraseña debe contener al menos un carácter especial" })
+})
+
+export function validateUser(object){
+    return validacionUsuario.safeParse(object);
+}
