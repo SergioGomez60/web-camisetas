edcd6dcf8d5dab202b952063eb3bc7c3347373e9
SergioGomez60 <sergio60gomez@gmail.com>
2025-11-21 07:06:36 +0100

    hhh

---
+++ frontend/src2/app/components/header/header.ts
@@ -0,0 +1,262 @@
+/*import { Component, AfterViewInit, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header implements OnInit {
+
+  ngOnInit() {
+
+    // MENU HAMBURGUESA
+    const menu = document.querySelector<HTMLImageElement>(".hamburguesa");
+    const secciones = document.querySelector(".secciones");
+
+    menu?.addEventListener("click", () => {
+      secciones?.classList.toggle("mostrar-secciones");
+
+      if(secciones?.classList.contains("mostrar-secciones")){
+        menu.src = "assets/x.png";
+      }else{
+        menu.src = "assets/menu-de-hamburguesas.png";
+      }
+    });
+
+    // MOSTRAR SUBSECCIONES
+  
+    const seccion = document.querySelectorAll(".seccion");
+
+    seccion.forEach(secc => {
+      secc.addEventListener("click",()=>{
+        const subsecciones = secc.querySelector(".subsecciones");
+        const flecha = secc.querySelector(".flecha");
+        if(subsecciones){
+          subsecciones.classList.toggle("mostrar-subsecciones");
+          flecha?.classList.toggle("rotar-flecha");
+        }
+      })
+    });
+
+
+
+    // MOSTRAR EQUIPOS
+  
+    const ligas = document.querySelectorAll(".liga");
+
+    ligas.forEach(liga => {
+      liga.addEventListener("click",(event)=>{
+        event.stopPropagation(); // evita que el click burbujee hacia .subsecciones
+        const equipos = liga.querySelector(".equipos");
+        const flechaDerecha = liga.querySelector(".flecha-derecha");
+        if(equipos){
+          equipos.classList.toggle("mostrar-equipos");
+          flechaDerecha?.classList.toggle("rotar-flecha270deg");
+        }
+      })
+    });
+
+  }
+}*/
+
+
+
+
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface Equipo { nombre: string }
+interface Liga { nombre: string; equipos: Equipo[]; abierta?: boolean; }
+interface Seccion { nombre: string; ligas?: Liga[]; abierta?: boolean; }
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+  imports: []
+})
+export class Header {
+  mostrarMenu = false;
+  secciones: Seccion[] = [];
+  mostrarBuscador = false;
+  mostrarCarrito = false;
+
+  constructor(private router:Router) {
+    this.secciones = [
+      {
+        nombre: "Clubes 25/26",
+        ligas: [
+          { nombre: "LaLiga", equipos: [
+            { nombre: 'FC Barcelona' },
+            { nombre: 'Real Madrid' },
+            { nombre: 'Villarreal CF' },
+            { nombre: 'Atlético de Madrid' },
+            { nombre: 'Real Betis' },
+            { nombre: 'RCD Espanyol' },
+            { nombre: 'Athletic Club' },
+            { nombre: 'Getafe CF' },
+            { nombre: 'Sevilla FC' },
+            { nombre: 'Deportivo Alavés' },
+            { nombre: 'Elche CF' },
+            { nombre: 'Rayo Vallecano' },
+            { nombre: 'Celta de Vigo' },
+            { nombre: 'Real Sociedad' },
+            { nombre: 'RCD Mallorca' },
+            { nombre: 'CA Osasuna' },
+            { nombre: 'Valencia CF' },
+            { nombre: 'Girona FC' },
+            { nombre: 'Levante UD' },
+            { nombre: 'Real Oviedo' }
+          ]},
+          { nombre: "Premier League", equipos: [
+            { nombre: 'Arsenal' },
+            { nombre: 'Manchester City' },
+            { nombre: 'Chelsea' },
+            { nombre: 'Sunderland' },
+            { nombre: 'Tottenham Hotspur' },
+            { nombre: 'Aston Villa' },
+            { nombre: 'Manchester United' },
+            { nombre: 'Liverpool' },
+            { nombre: 'Bournemouth' },
+            { nombre: 'Crystal Palace' },
+            { nombre: 'Brighton & Hove Albion' },
+            { nombre: 'Brentford' },
+            { nombre: 'Everton' },
+            { nombre: 'Newcastle United' },
+            { nombre: 'Fulham' },
+            { nombre: 'Leeds United' },
+            { nombre: 'Burnley' },
+            { nombre: 'West Ham United' },
+            { nombre: 'Nottingham Forest' },
+            { nombre: 'Wolves' }
+
+          ]},
+          { nombre: "Serie A", equipos: [
+            { nombre: 'Atalanta' },
+            { nombre: 'Bologna' },
+            { nombre: 'Cagliari' },
+            { nombre: 'Como' },
+            { nombre: 'Cremonese' },
+            { nombre: 'Fiorentina' },
+            { nombre: 'Genoa' },
+            { nombre: 'Hellas Verona' },
+            { nombre: 'Inter Milan' },
+            { nombre: 'Juventus' },
+            { nombre: 'Lazio' },
+            { nombre: 'Lecce' },
+            { nombre: 'AC Milan' },
+            { nombre: 'Napoli' },
+            { nombre: 'Parma' },
+            { nombre: 'Pisa' },
+            { nombre: 'Roma' },
+            { nombre: 'Sassuolo' },
+            { nombre: 'Torino' },
+            { nombre: 'Udinese' }
+          ]},
+          { nombre: "Bundesliga", equipos: [
+            { nombre: 'Bayern München' },
+            { nombre: 'RB Leipzig' },
+            { nombre: 'Borussia Dortmund' },
+            { nombre: 'VfB Stuttgart' },
+            { nombre: 'Bayer Leverkusen' },
+            { nombre: 'TSG Hoffenheim' },
+            { nombre: 'Eintracht Frankfurt' },
+            { nombre: 'Werder Bremen' },
+            { nombre: 'Köln' },
+            { nombre: 'SC Freiburg' },
+            { nombre: 'Union Berlin' },
+            { nombre: 'Borussia Mönchengladbach' },
+            { nombre: 'Hamburger SV' },
+            { nombre: 'VfL Wolfsburg' },
+            { nombre: 'FC Augsburg' },
+            { nombre: 'FC St. Pauli' },
+            { nombre: 'Mainz' },
+            { nombre: 'Heidenheim' }
+          ]},
+          { nombre: "Ligue 1", equipos: [
+            { nombre: 'Paris Saint-Germain' },
+            { nombre: 'Marseille' },
+            { nombre: 'RC Lens' },
+            { nombre: 'Strasbourg' },
+            { nombre: 'Lille' },
+            { nombre: 'AS Monaco' },
+            { nombre: 'Lyon' },
+            { nombre: 'Rennes' },
+            { nombre: 'Nice' },
+            { nombre: 'Toulouse' },
+            { nombre: 'Paris FC' },
+            { nombre: 'Le Havre' },
+            { nombre: 'Angers' },
+            { nombre: 'Metz' },
+            { nombre: 'Brest' },
+            { nombre: 'Nantes' },
+            { nombre: 'Lorient' },
+            { nombre: 'Auxerre' }
+          ]}
+        ]
+      },
+      {
+        nombre: "Selecciones 25/26",
+        ligas: [
+          { nombre: 'España', equipos:[] },
+          { nombre: 'Argentina' , equipos:[] },
+          { nombre: 'Francia' , equipos:[]},
+          { nombre: 'Inglaterra' , equipos:[] },
+          { nombre: 'Portugal' , equipos:[] },
+          { nombre: 'Países Bajos' , equipos:[] },
+          { nombre: 'Brasil' , equipos:[] },
+          { nombre: 'Bélgica' , equipos:[] },
+          { nombre: 'Croacia' , equipos:[] },
+          { nombre: 'Italia' , equipos:[] }
+        ]
+      },
+      { nombre: 'Camisetas Retro' },
+      { nombre: 'Cajas Sorpresa' }
+    ];
+  }
+
+  toggleMenu() {
+    this.mostrarMenu = !this.mostrarMenu;
+    this.mostrarBuscador = false 
+    this.mostrarCarrito = false
+  }
+
+
+  toggleSeccion(seccion: Seccion) {
+      this.secciones.forEach(s => { if (s !== seccion) s.abierta = false; });
+      seccion.abierta = !seccion.abierta;
+  }
+
+  toggleLiga(seccion: Seccion, liga: Liga) {
+    seccion.ligas?.forEach(l => {
+      if (l !== liga) l.abierta = false;
+    });
+    liga.abierta = !liga.abierta;
+  }
+
+  toggleBuscador(){
+    this.mostrarBuscador = !this.mostrarBuscador
+    this.mostrarMenu = false 
+    this.mostrarCarrito = false
+  }
+
+  toggleCarrito(){
+    this.mostrarCarrito = !this.mostrarCarrito
+    this.mostrarBuscador = false 
+    this.mostrarMenu = false
+  }
+
+  paginaLogin(){
+    this.router.navigate(["/login"]);
+  }
+
+ 
+}
+
+
+
+
+
+
+
+
