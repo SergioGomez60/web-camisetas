d42f4ee393ee530cb54e6097fefc6f9c32a400fd
SergioGomez60 <sergio60gomez@gmail.com>
2025-11-24 18:14:44 +0100

    borrado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,9 @@ import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
 import { Login } from './pages/login/login';
 import { Registro } from './pages/registro/registro';
-import { Autenticacion } from './pages/autenticacion/autenticacion';
 
 export const routes: Routes = [
     {path: "", component:Inicio },
     {path:"login", component:Login},
-    {path:"registro", component:Registro},
-    {path:"autenticacion", component:Autenticacion}
+    {path:"registro", component:Registro}
 ];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,19 +3,11 @@ import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
-import { provideAuth0 } from '@auth0/auth0-angular';
 
 bootstrapApplication(App, {
   providers: [
     provideRouter(routes),
-    provideAuth0({
-      domain: 'dev-32mzzfaff4uprtnv.us.auth0.com',
-      clientId: 'T7ZO8gLy6JiD0DMT0zOGMkeIxLf5nGtf',
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-        audience: "http://camisworld"
-      }
-    })
+    
   ]
 });
 
