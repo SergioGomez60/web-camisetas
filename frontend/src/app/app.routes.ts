e2ff46e2995fa64fb64ab97c34473ae0c3dd039d
SergioGomez60 <sergio60gomez@gmail.com>
2025-11-26 10:54:35 +0100

    elimino login y registro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { Inicio } from './pages/inicio/inicio';
-import { Login } from './pages/login/login';
-import { Registro } from './pages/registro/registro';
 import { Auth } from './components/auth/auth';
 import { authGuard } from './guards/auth-guard';
 
