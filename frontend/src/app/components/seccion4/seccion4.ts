b3797e80dc5c6025c71420f51edb0ae65a3b6d4a
SergioGomez60 <sergio60gomez@gmail.com>
2025-12-01 16:24:18 +0100

    borrar

--- frontend/src/app/components/seccion4/seccion4.ts
+++ frontend/src/app/components/seccion4/seccion4.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { SwiperModule } from 'swiper/angular';
-import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
 
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
 
 @Component({
   selector: 'app-seccion4',
   standalone: true,
-  imports: [SwiperModule],
+  imports: [],
   templateUrl: './seccion4.html',
   styleUrl: './seccion4.css',
 })
