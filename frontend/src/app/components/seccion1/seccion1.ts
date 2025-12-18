a2b3e1642b8d0626e35846a0cdefe358da68be4f
SergioGomez60 <sergio60gomez@gmail.com>
2025-12-18 07:47:42 +0100

    cambios

--- frontend/src/app/components/seccion1/seccion1.ts
+++ frontend/src/app/components/seccion1/seccion1.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 })
 export class Seccion1 {
   camisetas = [
-    {equipo: "FC Barcelona",descripcion:"Camiseta del FC Barcelona 2025/2026",precio:89.99,imagen:"assets/barcelona.jpg"},
+    {equipo: "FC Barcelona",descripcion:"Camiseta del FC Barcelona 2025/2026",precio:89.99,imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1765387569/250997_1_sr4eoy.webp"},
     {equipo: "Real Madrid",descripcion:"Camiseta del Real Madrid CF 2025/2026",precio:89.99,imagen:"assets/madrid.jpg"},
     {equipo: "Liverpool FC",descripcion:"Camiseta del Liverpool FC 2025/2026",precio:89.99,imagen:"assets/liverpool.jpg"},
     {equipo: "Arsenal",descripcion:"Camiseta del Arsenal FC 2025/2026",precio:89.99,imagen:"assets/arsenal.jpg"}
