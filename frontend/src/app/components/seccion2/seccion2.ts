f70d5c6ce867032727b619ef85997615899ec8f7
SergioGomez60 <sergio60gomez@gmail.com>
2025-12-30 07:52:03 +0100

    cambios

--- frontend/src/app/components/seccion2/seccion2.ts
+++ frontend/src/app/components/seccion2/seccion2.ts
@@ -9,10 +9,10 @@ import { RouterLink } from "@angular/router";
 })
 export class Seccion2 {
   cajas = [
-    {nombre: "Caja Low Cost", precio: 39.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123589/caja-lowcost_qjvddn.png"},
-    {nombre: "Caja Standard", precio: 49.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123588/caja-standard_cbyv1j.png"},
-    {nombre: "Caja Premium", precio: 69.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123590/caja-premium_lg4lb9.png"},
-    {nombre: "Caja Retro", precio: 79.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123589/caja-retro_lzjmxm.png"}
+    {id:1, nombre: "Caja Low Cost", precio: 39.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123589/caja-lowcost_qjvddn.png"},
+    {id:2, nombre: "Caja Standard", precio: 49.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123588/caja-standard_cbyv1j.png"},
+    {id:3, nombre: "Caja Premium", precio: 69.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123590/caja-premium_lg4lb9.png"},
+    {id:4, nombre: "Caja Retro", precio: 79.99, imagen:"https://res.cloudinary.com/dgxtoqvte/image/upload/v1766123589/caja-retro_lzjmxm.png"}
   ]
 
   
